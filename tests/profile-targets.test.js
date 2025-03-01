4e7fe4609692cac4b64faf7b1334bf0feee60f3c
Sam King <kingst@gmail.com>
2025-03-01 15:13:45 -0800

    fix profile tests

--- tests/profile-targets.test.js
+++ tests/profile-targets.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var should = require('should');
+var moment = require('moment');
 var _ = require('lodash');
 var targets = require('../lib/profile/targets');
 
@@ -21,7 +22,7 @@ describe('Target Profile', function() {
 		targetTop: 100,
 		targetBottom: 80,
 		duration: 120,
-		created_at: new Date('2025-01-26T02:00:00')
+		created_at: moment('2016-06-13 02:00:00').toDate()
             }
 	]);
     }
@@ -29,28 +30,28 @@ describe('Target Profile', function() {
     var profile = {};
 
     it('should return correct target from schedule', function() {
-        var now = new Date('2025-01-26T01:00:00');
+        var now = moment('2016-06-13 01:00:00').toDate();
         var result = targets.lookup({targets: target_input(), temptargets: temptargets_input()}, profile, now);
         result.high.should.equal(100);
         result.low.should.equal(100);
     });
 
     it('should use the profile override', function() {
-        var now = new Date('2025-01-26T01:00:00');
+        var now = moment('2016-06-13 01:00:00').toDate();
         var result = targets.lookup({targets: target_input(), temptargets: temptargets_input()}, {target_bg: 110}, now);
         result.high.should.equal(110);
         result.low.should.equal(110);
     });
 
     it('should handle target schedule changes', function() {
-        var now = new Date('2025-01-26T04:00:00');
+        var now = moment('2016-06-13 04:00:00').toDate();
         var result = targets.lookup({targets: target_input(), temptargets: temptargets_input()}, profile, now);
         result.high.should.equal(90);
         result.low.should.equal(90);
     });
 
     it('should handle temp targets', function() {
-        var now = new Date('2025-01-26T02:30:00'); // Within temp target duration
+        var now = moment('2016-06-13 02:30:00').toDate(); // Within temp target duration
         var result = targets.lookup({targets: target_input(), temptargets: temptargets_input()}, profile, now);
         result.high.should.equal(100);
         result.low.should.equal(80);
@@ -64,7 +65,7 @@ describe('Target Profile', function() {
             duration: 0,
             created_at: new Date('2025-01-26T02:30:00')
         }];
-        var now = new Date('2025-01-26T02:45:00');
+        var now = moment('2016-06-13 02:45:00').toDate();
         var result = targets.lookup({targets: target_input(), temptargets: cancel_temptarget}, profile, now);
         result.high.should.equal(100);
         result.low.should.equal(100);
